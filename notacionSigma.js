c641b5ece99c43cbfc4695434a699d20e8c578e9
erickArita <erick.emao@gmail.com>
2020-07-23 21:10:17 -0600

    formula defectuosa

--- notacionSigma.js
+++ notacionSigma.js
@@ -1,39 +1,48 @@
 const encendido = true
+// ciclo while (cuando)  para que si introduce mal un dato detiene el programa con el breack
 while (encendido == true) {
     // definimos variables  let para definir variables
 
     let z, a, n, y;
-    
+
     // para escribir en consola
 
     console.log('Sumatoria con Notación Sigma')
 
-    // prompt() para recibir la entrada
+    // prompt() para recibir la entrada y la guardamos en la variable
     z = prompt('introduzca a Z')
-
+    
     // validamos que los datos sean de acuerdo a las reglas
-
+    // esta es la condicional  (si  entonces)
     if (z < 1) {
         console.log('Z no puede ser menor que 1')
         // break finaliza el programa
-        break
+        break;
     }
-
     a = prompt('introduzca A');
-    n = prompt('Introduzca N');
-
+    
+    n = prompt('Introduzca N ,no debe se menor que a');
+    
     if (n > a || n < 0) {
         console.log('N no puede ser mayor que A ni menor que cero')
-        break
+        break;
     }
+    y = prompt('introduzca Y ,debe ser mayor que 0 y mayor que n');
 
-    y = prompt('introduzca Y');
     if (y < 0 || y < n) {
         console.log('Y no puede ser menor que 0 ni menor que N')
-        break
+        break;
     }
+    // ciclo para 
+    for (let i = 1; i < z; i++) {
 
-    
+
+
+        let resultado = (y + n ^ z / n * i-- ^ y)^a
+
+         
+        console.log(resultado)
+    }
 
 }
 
