cf3bed626ab23f39ac238244f8bcca7dee367d56
erickArita <erick.emao@gmail.com>
2020-07-23 21:51:36 -0600

    modificando formula

--- notacionSigma.js
+++ notacionSigma.js
@@ -1,48 +1,46 @@
-const encendido = true
-// ciclo while (cuando)  para que si introduce mal un dato detiene el programa con el breack
-while (encendido == true) {
-    // definimos variables  let para definir variables
+// definimos variables  let para definir variables
 
-    let z, a, n, y;
+let z, a, n, y;
 
-    // para escribir en consola
+// para escribir en consola
 
-    console.log('Sumatoria con Notación Sigma')
+console.log('Sumatoria con Notación Sigma')
 
-    // prompt() para recibir la entrada y la guardamos en la variable
-    z = prompt('introduzca a Z')
-    
-    // validamos que los datos sean de acuerdo a las reglas
-    // esta es la condicional  (si  entonces)
-    if (z < 1) {
-        console.log('Z no puede ser menor que 1')
-        // break finaliza el programa
-        break;
-    }
+// prompt() para recibir la entrada y la guardamos en la variable
+z = prompt('introduzca a Z')
+
+// validamos que los datos sean de acuerdo a las reglas
+// esta es la condicional  (si  entonces)
+if (z < 1) {
+    console.log('Z no puede ser menor que 1')
+    // break finaliza el programa
+
+} else {
     a = prompt('introduzca A');
-    
-    n = prompt('Introduzca N ,no debe se menor que a');
-    
-    if (n > a || n < 0) {
-        console.log('N no puede ser mayor que A ni menor que cero')
-        break;
-    }
-    y = prompt('introduzca Y ,debe ser mayor que 0 y mayor que n');
-
-    if (y < 0 || y < n) {
-        console.log('Y no puede ser menor que 0 ni menor que N')
-        break;
-    }
-    // ciclo para 
-    for (let i = 1; i < z; i++) {
-
-
-
-        let resultado = (y + n ^ z / n * i-- ^ y)^a
-
-         
-        console.log(resultado)
-    }
+    n = prompt('Introduzca N ,debe ser menor que a');
 
 }
+
+
+if (n > a || n < 0) {
+    console.log('N no puede ser mayor que A ni menor que cero')
+
+    y = prompt('introduzca Y ,debe ser mayor que 0 y mayor que n');
+}
+
+if (y < 0 || y < n) {
+    console.log('Y no puede ser menor que 0 ni menor que N')
+
+}
+// ciclo para 
+for (let i = 1; i < z; i++) {
+
+    
+    let resultado = (y + n ^ z / n * --i ^ y) ^ a
+
+
+    console.log(resultado)
+}
+
+
 
