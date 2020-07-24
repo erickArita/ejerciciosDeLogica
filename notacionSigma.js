dfe9a0e22bcabb9d40fdffac5c72f3dcd46c830b
erickArita <erick.emao@gmail.com>
2020-07-23 20:22:51 -0600

    pedir datos al usuario

--- notacionSigma.js
+++ notacionSigma.js
@@ -1,1 +1,31 @@
-console.log('sumatoria con notación sigma')
+// definimos variables  let para definir variables
+let z, a, n, y;
+// para escribir en consola
+console.log('Sumatoria con Notación Sigma')
+
+// prompt() para recibir la entrada
+z = prompt('introduzca a Z')
+
+// validamos que los datos sean de acuerdo a las reglas
+if (z < 1) {
+    console.log('z no puede ser menor que 1')
+    // return finaliza el programa
+      
+}
+
+a = prompt('introduzca A');
+n = prompt('Introduzca N')
+if (n > a || n < 0) {
+    console.log('N no puede ser mayor que A ni menor que cero')
+
+}
+
+y = prompt('introduzca Y');
+if (y < 0 || y < n) {
+    console.log('y no puede ser menor que 0 ni menor que n')
+
+}
+
+
+
+
